ba9785469f5bad707689c17f9061e2488af65aec
John Doe <j.caceresc@uniandes.edu.co>
2022-10-29 17:21:15 -0500

    creation client

---
+++ Caso3/seguridad20222_servidor/src/cliente/ClienteThread.java
@@ -0,0 +1,11 @@
+package cliente;
+
+import java.math.BigInteger;
+import java.security.PrivateKey;
+
+public class ClienteThread {
+    private PrivateKey privadaServidor;
+    private BigInteger p;
+    private BigInteger g;
+
+}
